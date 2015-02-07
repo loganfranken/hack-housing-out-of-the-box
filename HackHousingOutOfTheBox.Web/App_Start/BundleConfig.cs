75e6fe0d9e7966604308b023c8be9f5673dff448
Logan Franken <loganfranken@gmail.com>
2015-02-07 11:50:12 -0800

    Renamed project to proper web namespace

--- HackHousingOutOfTheBox.Web/App_Start/BundleConfig.cs
+++ HackHousingOutOfTheBox.Web/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace HackHousingOutOfTheBox
+namespace HackHousingOutOfTheBox.Web
 {
     public class BundleConfig
     {
--- HackHousingOutOfTheBox.Web/App_Start/WebApiConfig.cs
+++ HackHousingOutOfTheBox.Web/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace HackHousingOutOfTheBox
+namespace HackHousingOutOfTheBox.Web
 {
     public static class WebApiConfig
     {
