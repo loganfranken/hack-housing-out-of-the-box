c99e9a1f43c76d5530b210f2539fa1700ef8759d
Logan Franken <loganfranken@gmail.com>
2015-02-07 11:43:23 -0800

    Set up initial MVC project

---
+++ HackHousingOutOfTheBox/App_Start/AuthConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+
+namespace HackHousingOutOfTheBox
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+        }
+    }
+}
