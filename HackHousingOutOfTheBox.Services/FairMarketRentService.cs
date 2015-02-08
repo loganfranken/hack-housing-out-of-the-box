68d1c2ebdcb46f08fedc5130121b412024f2fa8a
Logan Franken <loganfranken@gmail.com>
2015-02-07 19:20:34 -0800

    Swapped latitude and longitude in FMR query

--- HackHousingOutOfTheBox.Services/FairMarketRentService.cs
+++ HackHousingOutOfTheBox.Services/FairMarketRentService.cs
@@ -14,7 +14,7 @@ namespace HackHousingOutOfTheBox.Services
     {
         public FairMarketRentInfo GetFairMarketRent(string latitude, string longitude)
         {
-            string url = String.Format("http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/FairMarketRents/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=%7B%22x%22%3A+{0}%2C+%22y%22%3A+{1}%2C+%22spatialReference%22%3A+%7B%22wkid%22%3A+4326%7D%7D&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pjson&token=", latitude, longitude);    
+            string url = String.Format("http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/FairMarketRents/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=%7B%22x%22%3A+{0}%2C+%22y%22%3A+{1}%2C+%22spatialReference%22%3A+%7B%22wkid%22%3A+4326%7D%7D&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pjson&token=", longitude, latitude);    
             HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
 
             using(HttpWebResponse response = (HttpWebResponse) request.GetResponse())
