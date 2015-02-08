5abfea0dd545269cd8dc33b7ee6fef515826eaba
Logan Franken <loganfranken@gmail.com>
2015-02-07 20:16:24 -0800

    Added basic public housing authority service

--- HackHousingOutOfTheBox.Services/FairMarketRentService.cs
+++ HackHousingOutOfTheBox.Services/FairMarketRentService.cs
@@ -12,27 +12,33 @@ namespace HackHousingOutOfTheBox.Services
 {
     public class FairMarketRentService
     {
-        public FairMarketRentInfo GetFairMarketRent(string latitude, string longitude)
+        public FairMarketRentInfo GetFairMarketRentInfo(string latitude, string longitude)
         {
-            string url = String.Format("http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/FairMarketRents/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=%7B%22x%22%3A+{0}%2C+%22y%22%3A+{1}%2C+%22spatialReference%22%3A+%7B%22wkid%22%3A+4326%7D%7D&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pjson&token=", longitude, latitude);    
-            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            var response = RequestByCoordinate("VTyQ9soqVukalItT", "FairMarketRents", latitude, longitude);
 
-            using(HttpWebResponse response = (HttpWebResponse) request.GetResponse())
-            using(StreamReader stream = new StreamReader(response.GetResponseStream()))
+            return new FairMarketRentInfo()
+            {
+                Efficiency = (int)response["FMR_0BDR"].Value,
+                OneBedroom = (int)response["FMR_1BDR"].Value,
+                TwoBedrooms = (int)response["FMR_2BDR"].Value,
+                ThreeBedrooms = (int)response["FMR_3BDR"].Value,
+                FourBedrooms = (int)response["FMR_4BDR"].Value
+            };
+        }
+
+        public static dynamic RequestByCoordinate(string apiId, string apiName, string latitude, string longitude)
+        {
+            string url = String.Format("http://services.arcgis.com/{0}/arcgis/rest/services/{1}/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=%7B%22x%22%3A+{2}%2C+%22y%22%3A+{3}%2C+%22spatialReference%22%3A+%7B%22wkid%22%3A+4326%7D%7D&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pjson&token=", apiId, apiName, longitude, latitude);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (StreamReader stream = new StreamReader(response.GetResponseStream()))
             {
                 string jsonResponse = stream.ReadToEnd();
                 var result = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
 
-                var attributes = result["features"][0]["attributes"];
-
-                FairMarketRentInfo info = new FairMarketRentInfo();
-                info.Efficiency = (int) attributes["FMR_0BDR"].Value;
-                info.OneBedroom = (int) attributes["FMR_1BDR"].Value;
-                info.TwoBedrooms = (int) attributes["FMR_2BDR"].Value;
-                info.ThreeBedrooms = (int) attributes["FMR_3BDR"].Value;
-                info.FourBedrooms = (int) attributes["FMR_4BDR"].Value;
-
-                return info;
+                return result["features"][0]["attributes"];
             }
         }
     }
---
+++ HackHousingOutOfTheBox.Services/PublicHousingAuthorityInfoService.cs
@@ -0,0 +1,44 @@
+﻿using Esri.ArcGISRuntime.Tasks.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackHousingOutOfTheBox.Services
+{
+    public class PublicHousingAuthorityInfoService
+    {
+        public PublicHousingAuthorityInfo GetPublicHousingAuthorityInfo(string city, string state)
+        {
+            return GetPublicHousingAuthorityInfoAsync(city, state).Result;
+        }
+
+        private async Task<PublicHousingAuthorityInfo> GetPublicHousingAuthorityInfoAsync(string city, string state)
+        {
+            var uri = "http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/PublicHousingAuthorities/FeatureServer/0/";
+            var queryTask = new QueryTask(new Uri(uri));
+
+            var timeWindow = new Esri.ArcGISRuntime.Data.TimeExtent(DateTime.Now.Subtract(new TimeSpan(0, 5, 0, 0)), DateTime.Now);
+
+            var queryParams = new Esri.ArcGISRuntime.Tasks.Query.Query(timeWindow);
+            queryParams.Where = String.Format("(STD_CITY = '{0}' AND STD_ST = '{1}')", city, state);
+            queryParams.OutFields = OutFields.All;
+
+            QueryResult queryResult = await queryTask.ExecuteAsync(queryParams);
+
+            var attributes = queryResult.FeatureSet.Features[0].Attributes;
+
+            return new PublicHousingAuthorityInfo
+            {
+                Name = (string) attributes["FORMAL_PARTICIPANT_NAME"],
+                Address = (string)attributes["STD_ADDR"],
+                Phone = (string)attributes["EXEC_DIR_PHONE"],
+                City = (string)attributes["STD_CITY"],
+                State = (string)attributes["STD_ST"],
+                PostalCode = (string)attributes["STD_ZIP5"],
+                Email = (string)attributes["EXEC_DIR_EMAIL"]
+            };
+        }
+    }
+}
